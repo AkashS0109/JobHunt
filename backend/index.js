cb40ea523dd2e3cc1b7ccaaa6900e18af55ece3c
Akash <akashsingh200056@gmail.com>
2024-12-25 12:46:53 +0530

    debug

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-     origin: ["http://localhost:5173","https://job-hunt-eosin.vercel.app/"],
+     origin: ["http://localhost:5173","https://job-hunt-eosin.vercel.app","https://jobhunt-n68a.onrender.com"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Allow-Credentials'],
     credentials: true,

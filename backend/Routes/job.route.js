07f549ff30010890892ba5ce246817e961968511
Akash <akashsingh200056@gmail.com>
2024-12-25 20:47:15 +0530

    debug

--- backend/Routes/job.route.js
+++ backend/Routes/job.route.js
@@ -4,7 +4,7 @@ import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/jo
 const router = express.Router();
 
 router.route("/post").post(isAuthenticated,postJob); //aaautheticated bcz only autheticated uer can create job 
-router.route("/get").get(isAuthenticated,getAllJobs);    // post bcz we r sending data
+router.route("/get").get(getAllJobs);    // post bcz we r sending data
 router.route("/getadminjobs").get(isAuthenticated,getAdminJobs);  //get bcz we r not ending any data
 router.route("/get/:id").get(isAuthenticated,getJobById);
 
